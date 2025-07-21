f6db55eebea6e9d57d6613c1d0df723c8c2034da
elSapitoWai <sapitowai@proton.me>
2025-07-21 19:18:58 +0200

    Alias for eza

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias ls="eza -a -T -L=2 -l"
 end
 
 oh-my-posh init fish --config ~/.config/omp/base.omp.toml | source
