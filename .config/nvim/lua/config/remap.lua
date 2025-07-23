75fbe3dbdd77e99da128e1c0121baefe451b5f02
elSapitoWai <sapitowai@proton.me>
2025-07-23 10:52:12 +0200

    Neovim blink config

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -11,3 +11,6 @@ vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
 vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,89 +1,53 @@
 return {
-	"saghen/blink.cmp",
-	version = not vim.g.lazyvim_blink_main and "*",
-	build = vim.g.lazyvim_blink_main and "cargo build --release",
-	opts_extend = {
-		"sources.completion.enabled_providers",
-		"sources.compat",
-		"sources.default",
-	},
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-		-- add blink.compat to dependencies
-		{
-			"saghen/blink.compat",
-			optional = true, -- make optional so it's only enabled if any extras need it
-			opts = {},
-			version = not vim.g.lazyvim_blink_main and "*",
-		},
-	},
-	opts = {
-		snippets = {
-			expand = function(snippet, _)
-				return LazyVim.cmp.expand(snippet)
-			end,
-		},
-		appearance = {
-			-- sets the fallback highlight groups to nvim-cmp's highlight groups
-			-- useful for when your theme doesn't support blink.cmp
-			-- will be removed in a future release, assuming themes add support
-			use_nvim_cmp_as_default = false,
-			-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- adjusts spacing to ensure icons are aligned
-			nerd_font_variant = "mono",
-		},
-		completion = {
-			accept = {
-				-- experimental auto-brackets support
-				auto_brackets = {
-					enabled = true,
-				},
-			},
-			menu = {
-				draw = {
-					treesitter = { "lsp" },
-				},
-			},
-			documentation = {
-				auto_show = true,
-				auto_show_delay_ms = 200,
-			},
-		},
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = { 'rafamadriz/friendly-snippets' },
 
-		-- experimental signature help support
-		-- signature = { enabled = true },
+  -- use a release tag to download pre-built binaries
+  version = '1.*',
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
 
-		sources = {
-			-- adding any nvim-cmp sources here will enable them
-			-- with blink.compat
-			compat = {},
-			default = { "lsp", "path", "snippets", "buffer" },
-		},
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'default' },
 
-		cmdline = {
-			enabled = false,
-		},
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
 
-		keymap = {
-			preset = "enter",
-			["<C-y>"] = { "select_and_accept" },
-		},
-	},
-	config = function(_, opts)
-		local enabled = opts.sources.default
-		for _, source in ipairs(opts.sources.compat or {}) do
-			opts.sources.providers[source] = vim.tbl_deep_extend(
-				"force",
-				{ name = source, module = "blink.compat.source" },
-				opts.sources.providers[source] or {}
-			)
-			if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
-				table.insert(enabled, source)
-			end
-		end
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = { documentation = { auto_show = false } },
 
-		opts.sources.compat = nil
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
 
-		require("blink.cmp").setup(opts)
-	end
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "prefer_rust_with_warning" }
+  },
+  opts_extend = { "sources.default" }
 }
